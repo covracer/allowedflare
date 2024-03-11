065981d90c9c7e95697b84724382e787b7ac4c91
Christopher Covington <cov@mykolab.com>
2024-03-11 09:37:34 -0400

    Try to test

--- allowedflare-proxy/index.test.ts
+++ allowedflare-proxy/index.test.ts
@@ -1,25 +1,31 @@
-import { afterAll, beforeAll, describe, expect, it } from "vitest";
-import { unstable_dev } from "wrangler";
-import type { UnstableDevWorker } from "wrangler";
+import { afterAll, beforeAll, describe, expect, it, vi } from 'vitest';
+import { unstable_dev } from 'wrangler';
+import type { UnstableDevWorker } from 'wrangler';
 
-describe("Worker", () => {
-	let worker: UnstableDevWorker;
+global.fetch = vi.fn()
+
+function fetchResponse(data) {
+    return {
+        json: () => new Promise((resolve) => resolve(data))
+    }
+}
+
+describe('Worker', () => {
+	let worker: UnstableDevWorker
 
 	beforeAll(async () => {
-		worker = await unstable_dev("index.ts", {
+		worker = await unstable_dev('allowedflare-proxy/index.ts', {
 			experimental: { disableExperimentalWarning: true },
-		});
-	});
+		})
+	})
 
 	afterAll(async () => {
-		await worker.stop();
-	});
+		await worker.stop()
+	})
 
-	it("should return Hello World", async () => {
-		const resp = await worker.fetch();
-		if (resp) {
-			const text = await resp.text();
-			expect(text).toMatchInlineSnapshot(`"Hello World!"`);
-		}
-	});
-});
+	it('should fetch the subdomain', async () => {
+        fetch.mockResolvedValue(fetchResponse({}))
+		await worker.fetch('https://subdomain.example.com/x/subbysub/path?query=string')
+        expect(fetch).toHaveBeenCalledWith('https://subbysub.example.com/path?query=string')
+	})
+})
